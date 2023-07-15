32a9903a0ae9404eedd8122d12789337fbb38485
Jasmine McManus <jasmine.mcmanus@outlook.com>
2023-07-15 17:38:12 +1000

    added requested updates post assignment review

--- script.js
+++ script.js
@@ -1,41 +1,87 @@
+// global variables
+
+let genreDisplay = document.getElementById('genre-display');
+let selectedGenre = document.getElementById('selected-genre');
+let form = document.querySelector('form');
+
+// event listeners for buttons
+
+const randomiseButton = document.querySelector('button.primary');
+    randomiseButton.addEventListener('click', function(){displayRandomBook()});
+
+const inputForm = document.querySelector('form');
+inputForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    let formGenre = document.querySelector('input#genre');
+    let cleanGenre = formGenre.value.replace(/ /g,"%2B")
+
+    displayRandomBook(cleanGenre);
+
+    genreDisplay.style.display = 'block';
+    selectedGenre.innerText = genre.value
+    }
+)
+
+const formButton = document.querySelector('button.secondary');
+    formButton.addEventListener('click', displayForm);
+
 // taking API data and rendering it on the page
 
-function displayGoogleBook() {
+function displayRandomBook(genre = 'fiction') {
+    genreDisplay.style.display = 'none';
+    form.style.display = 'none';
 
-    fetch('https://www.googleapis.com/books/v1/volumes?q=subject:fiction&maxResults=20&fields=items')
+    console.log(`genre is ${genre}`);
+
+    fetch(`https://www.googleapis.com/books/v1/volumes?q=subject:${genre}&maxResults=40&fields=items`)
+    
     .then(function(response) {
         return response.json();
     })
+
     .then(function(data) {
-        displayRandomGoogleBook(data);
+        selectRandomBook(data);
     })
+
 }
 
-function displayRandomGoogleBook(data) {
-    let form = document.querySelector('form');
-    form.style.display = 'none';
+// randomising API data, callback function
 
-    let max = data.items.length - 1;
-    let index = Math.floor(Math.random() * max );
+function selectRandomBook(data) {
 
-    let card = document.getElementById('book-display');
-    card.style.display = 'block';
-    
-    let cover = document.querySelector('#book-display img');
-    cover.src = `${data.items[index].volumeInfo.imageLinks.thumbnail}`;
+    if (typeof data.items === 'object') {
+        let max = data.items.length - 1;
+        let index = Math.floor(Math.random() * max );
 
-    let title = document.querySelector('#book-display h2');
-    title.innerText = data.items[index].volumeInfo.title;
+        let card = document.getElementById('book-display');
+        card.style.display = 'block';
         
-    let author = document.querySelector('#book-display h3');
-    author.innerText = data.items[index].volumeInfo.authors;
+        let cover = document.querySelector('#book-display img');
 
-    let description = document.querySelector('#book-display p');
-    description.innerText = data.items[index].volumeInfo.description;
+        if (typeof data.items[index].volumeInfo.imageLinks == 'undefined') {
+            cover.src = './src/no-cover-art.jpg'
+        }
+        else {
+            cover.src = `${data.items[index].volumeInfo.imageLinks.thumbnail}`;
+        }
+
+        let title = document.querySelector('#book-display h2');
+        title.innerText = data.items[index].volumeInfo.title;
+        
+        let author = document.querySelector('#book-display h3');
+        author.innerText = data.items[index].volumeInfo.authors;
+
+        let description = document.querySelector('#book-display p');
+        description.innerText = data.items[index].volumeInfo.description;
+    }
+    else {
+        handleErrors();
+    }
 
 }
 
-// the form to filter selections
+// displaying the form to filter selections
 
 function displayForm() {
     let form = document.querySelector('form')
@@ -47,22 +93,17 @@ function displayForm() {
 
 }
 
-const init = () => {
-    const inputForm = document.querySelector('form');
-  
-    inputForm.addEventListener('submit', (event) => {
-      event.preventDefault();
-      
-      let genre = document.querySelector('input#genre');
-      let cleanGenre = genre.value.replace(/ /g,"%2B")
-      console.log(cleanGenre)
+// handling errors
 
-      fetch(`https://books.googleapis.com/books/v1/volumes?q=subject:${cleanGenre}&maxResults=40&fields=items`)
-      .then(response => response.json())
-      .then(data => {
-        displayRandomGoogleBook(data);
-      });
-    });
-  }
+function handleErrors() {
+    let alertBox = document.querySelector('div.alert');
+    alertBox.style.display = 'block';
+}
 
-  document.addEventListener('DOMContentLoaded', init);
+
+let closeButton = document.querySelector('span.close-button');
+    closeButton.addEventListener('click', (closeFunction))
+
+function closeFunction() {
+    this.parentElement.style.display='none';
+}
